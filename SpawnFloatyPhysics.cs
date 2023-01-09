c819dda0fc06113fcc4c9fbd05b8292e8797b69c
donkeyrat <darklordbananas@gmail.com>
2023-01-08 18:23:12 -0800

    fixed bug with wings

--- SpawnFloatyPhysics.cs
+++ SpawnFloatyPhysics.cs
@@ -5,8 +5,24 @@ namespace VanillaPlus
 {
     public class SpawnFloatyPhysics : MonoBehaviour
     {
-        public void Awake()
+        public void Start()
         {
+            var wings = transform.root.GetComponentInChildren<Wings>();
+            if (wings && wings.useWingsInPlacement)
+            {
+                var standingHandler = transform.root.GetComponentInChildren<StandingHandler>();
+                if (standingHandler) standingHandler.enabled = true;
+                
+                return;
+            }
+
+            if (transform.root.GetComponent<Unit>())
+            {
+                var unit = transform.root.GetComponent<Unit>();
+                unit.data.footLeft.GetComponent<CurveAnimation>().multiplier = 2.5f;
+                unit.data.footRight.GetComponent<CurveAnimation>().multiplier = 2.5f;
+            }
+            
             Instantiate(VPMain.combatUpgrade.LoadAsset<GameObject>("2.0Hover"),
                 transform.parent);
             Destroy(this);
